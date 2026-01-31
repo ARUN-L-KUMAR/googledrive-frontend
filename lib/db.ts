e42846d5f8fb62895e39fbce8fa0087c9602e201
ARUN-L-KUMAR <arunkumar582004@gmail.com>
2026-02-01 02:04:56 +0530

    feat: setup MongoDB connection with Mongoose

---
+++ lib/db.ts
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+if (!process.env.MONGO_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGO_URI"');
+}
+
+const MONGO_URI = process.env.MONGO_URI;
+
+interface CachedConnection {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+let cached: CachedConnection = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
